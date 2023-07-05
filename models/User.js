0948776f1e23d2c7864f737f90d6a47c58d258a4
andrsvsrg <androsov.srg@gmail.com>
2023-07-05 12:38:12 +0300

    init with API instagram , no db

---
+++ models/User.js
@@ -0,0 +1,40 @@
+// const user = response.data.user
+// const userModel = {
+//   userInstId: user.id,   // string
+//   username: user.username,  // string
+//   biography: user.biography,  // string
+//   followed: user.edge_followed_by.count,  // number
+//   follow: user.edge_follow.count,  // number
+//   fullName: user.full_name,  // string
+//   categoryName: user.category_name, // string
+//   profilePicUrl: user.profile_pic_url_hd || user.profile_pic_url, // URL
+//   bioLinks: user.bio_links.map((link) => link.url).filter((e) => e)  // URL
+// }
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  userInstId: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  biography: String,  // string
+  followed:{
+    type: Number,
+    default: 0,
+  },
+  follow: {
+    type: Number,
+    default: 0,
+  },
+  fullName: String,
+  categoryName:  String, // string
+  profilePicUrl: URL,
+  bioLinks: Array  // URL
+})
